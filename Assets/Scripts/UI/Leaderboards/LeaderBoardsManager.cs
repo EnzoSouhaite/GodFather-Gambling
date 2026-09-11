5fbef2abca4843214de369ed47a2ff902bca696e
Auguste Paccapelo <paccapelo.auguste@gmail.com>
2026-09-11 11:28:31 +0200

    [ADD] Worst leader board

--- Assets/Scripts/UI/Leaderboards/LeaderBoardsManager.cs
+++ Assets/Scripts/UI/Leaderboards/LeaderBoardsManager.cs
@@ -10,15 +10,21 @@ public class LeaderBoardsManager : MonoBehaviour
     [SerializeField] private PlayerLastGame _prefabPlayerLast;
     [SerializeField] private Transform _lastContainer;
 
+    [SerializeField] private Transform _worstContainer;
+
     [SerializeField] private Image[] _podium;
     [SerializeField] private List<Sprite> _horsesSprites = new List<Sprite>();
+
+    private int playerCapGlob = 10;
+    private int playerCapLast = 10;
+    private int playerCapWorst = 5;
 
     private void Start()
     {
         List<SOPlayerInfo> allPlayers = AccountsManager.GetAllPlayers();
 
         allPlayers.Sort((a, b) => b.Balance.CompareTo(a.Balance));
-        allPlayers.Capacity = 10;
+        allPlayers.RemoveRange(playerCapGlob - 1, allPlayers.Count - playerCapGlob);
 
         PlayerGlobalLeader stat;
         foreach (SOPlayerInfo player in allPlayers)
@@ -30,13 +36,24 @@ public class LeaderBoardsManager : MonoBehaviour
         List<SOBetInfo> allBets = BetManager.GetAllBets();
 
         allBets.Sort((a, b) => b.LastAmountWon.CompareTo(a.LastAmountWon));
-        allBets.Capacity = 10;
+        allBets.RemoveRange(playerCapLast - 1, allBets.Count - playerCapLast);
 
         PlayerLastGame lastGame;
         foreach (SOBetInfo bet in allBets)
         {
             lastGame = Instantiate(_prefabPlayerLast, _lastContainer);
             lastGame.Init(bet.Name, bet.LastAmountWon);
+        }
+
+        List<SOPlayerInfo> worstPlayers = AccountsManager.GetAllPlayers();
+        worstPlayers.Sort((a, b) => a.Balance.CompareTo(b.Balance));
+        worstPlayers.RemoveRange(playerCapWorst - 1, worstPlayers.Count - playerCapWorst);
+
+        PlayerGlobalLeader worstStat;
+        foreach (SOPlayerInfo player in worstPlayers)
+        {
+            worstStat = Instantiate(_prefabPlayerGlobal, _worstContainer);
+            worstStat.Init(player.Name, player.Balance);
         }
 
         BetManager.ClearBets();
